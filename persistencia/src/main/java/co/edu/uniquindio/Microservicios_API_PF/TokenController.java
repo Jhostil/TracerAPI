f04b909abbf5f1d39b85096cb43d5d05c72d3659
DANIEL <daniel.molinam@uqvirtual.edu.co>
2023-05-04 08:26:21 -0500

    se actualizan los resultados del openapi

--- persistencia/src/main/java/co/edu/uniquindio/Microservicios_API_PF/TokenController.java
+++ persistencia/src/main/java/co/edu/uniquindio/Microservicios_API_PF/TokenController.java
@@ -2,17 +2,23 @@ package co.edu.uniquindio.Microservicios_API_PF;
 
 import co.edu.uniquindio.Microservicios_API_PF.entidades.Usuario;
 import co.edu.uniquindio.Microservicios_API_PF.servicios.TokenServicio;
+import co.edu.uniquindio.Microservicios_API_PF.servicios.UsuarioServicio;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/tokens")
 public class TokenController {
 
     private final TokenServicio tokenService;
+    private final UsuarioServicio usuarioServicio;
 
-    public TokenController(TokenServicio tokenService) {
+    public TokenController(TokenServicio tokenService, UsuarioServicio usuarioServicio) {
         this.tokenService = tokenService;
+        this.usuarioServicio = usuarioServicio;
     }
 
     @PostMapping("/generacion")
@@ -22,9 +28,13 @@ public class TokenController {
     }
 
     @GetMapping("/validacion")
-    public ResponseEntity<Boolean> validateToken(@RequestParam String tokenString, @RequestBody Usuario usuario) {
-        boolean isValid = tokenService.validateToken(tokenString, usuario);
-        return ResponseEntity.ok(isValid);
+    public ResponseEntity<Boolean> validateToken(@RequestParam String tokenString, @RequestParam String correo) {
+        Optional<Usuario> usuario = usuarioServicio.obtenerUsuarioPorCorreo(correo);
+        if(usuario.isPresent()){
+            boolean isValid = tokenService.validateToken(tokenString, usuario.get());
+            return ResponseEntity.ok(isValid);
+        }else
+            throw new RuntimeException("El usuario buscado no ha sido encontrado");
     }
 
     @GetMapping("/sujeto")
